f580405161a91e953c130b56a381218243560938
Hoang <nguyenhuyhoang112k@gmail.com>
2022-09-08 15:05:54 +0700

    sidebar1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,6 +6,7 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 //layout
 import { HeaderOnly } from '~/layouts';
@@ -13,6 +14,7 @@ import { HeaderOnly } from '~/layouts';
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.Upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
